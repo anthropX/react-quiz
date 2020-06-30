000f5711b03d808a9ce316df1714c014ecfe82be
Vimal Patel <94vimalpatel@gmail.com>
2020-06-30 23:00:42 +0530

    feat: add the info card component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../App.css'
 import ProgressBar from './ProgressBar'
+import InfoCard from './InfoCard'
 export default App
 
 function App () {
   return (
     <>
-      <ProgressBar />
-      {/* <InfoCard/>
-    <QuestionCard/>
+    <ProgressBar />
+    <InfoCard/>
+    {/*<QuestionCard/>
     <ScoreCard/> */}
     </>
   )
---
+++ src/components/InfoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default InfoCard
+
+function InfoCard () {
+  return (
+    <div id='info_card'>
+      <h2 id='no_of_questions_answered_text'>Question 0 of 20</h2>
+      <p id='question_category_text'>Entertainment: Video Games</p>
+      <div id='question_difficulty_indicator' />
+    </div>
+  )
+}
