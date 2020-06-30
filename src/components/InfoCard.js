dd69c6f1f59745447d05fca546215c3f7153aab7
Vimal Patel <94vimalpatel@gmail.com>
2020-06-30 23:05:25 +0530

    feat: add the question difficulty indicator

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -6,7 +6,12 @@ function InfoCard () {
     <div id='info_card'>
       <h2 id='no_of_questions_answered_text'>Question 0 of 20</h2>
       <p id='question_category_text'>Entertainment: Video Games</p>
-      <div id='question_difficulty_indicator' />
+      <div id='question_difficulty_indicator' >
+      <i class="fas fa-star highlighted"></i>
+      <i class="fas fa-star highlighted"></i>
+      <i class="fas fa-star"></i>
+      </div>
+
     </div>
   )
 }
