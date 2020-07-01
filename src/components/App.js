a64b92a4095908a3c90777cb0058003c1ca60992
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 11:32:58 +0530

    feat: size progress fill based on prop value

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import ProgressBar from './ProgressBar'
 import InfoCard from './InfoCard'
@@ -7,9 +7,10 @@ import ScoreCard from './ScoreCard'
 export default App
 
 function App () {
+  const [noOfQuestionsAnswered, setNoOfQuestionsAnswered] = useState(0)
   return (
     <>
-      <ProgressBar />
+      <ProgressBar progressPercentage={(noOfQuestionsAnswered / 20) * 100} />
       <InfoCard />
       <QuestionCard />
       <ScoreCard />
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 export default ProgressBar
 
-function ProgressBar () {
+function ProgressBar ({ progressPercentage }) {
   return (
     <div id='progress_bar'>
-      <div id='progress_bar_fill' />
+      <div
+        id='progress_bar_fill'
+        style={{ width: progressPercentage + '%' }}
+      />
     </div>
   )
+}
+
+ProgressBar.propTypes = {
+  progressPercentage: PropTypes.number
 }
