24aecd3534c622445ae07810755194a7bcde104c
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 12:52:15 +0530

    test: make sure App has all child components

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
+import ProgressBar from '../ProgressBar'
+import InfoCard from '../InfoCard'
+import QuestionCard from '../QuestionCard'
+import ScoreCard from '../ScoreCard'
 
 let wrapper
 
@@ -10,4 +14,20 @@ beforeEach(() => {
 
 it('renders', () => {
   expect(wrapper.exists()).toBe(true)
+})
+
+it('has the progress bar', () => {
+  expect(wrapper.find(ProgressBar).length).toEqual(1)
+})
+
+it('has the info card', () => {
+  expect(wrapper.find(InfoCard).length).toEqual(1)
+})
+
+it('has the question card', () => {
+  expect(wrapper.find(QuestionCard).length).toEqual(1)
+})
+
+it('has the score card', () => {
+  expect(wrapper.find(ScoreCard).length).toEqual(1)
 })
