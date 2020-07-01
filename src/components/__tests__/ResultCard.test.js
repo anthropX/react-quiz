287ed883acea6280f3f6c822d4d19fda78991e47
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 18:54:58 +0530

    refactor: move result card test above result text

--- src/components/__tests__/ResultCard.test.js
+++ src/components/__tests__/ResultCard.test.js
@@ -12,18 +12,18 @@ it('renders', () => {
   expect(wrapper.exists()).toBe(true)
 })
 
-it('displays result text from prop', () => {
-  expect(wrapper.find('#result_text').text()).toEqual('')
-  wrapper.setProps({ resultText: 'Correct!' })
-  expect(wrapper.find('#result_text').text()).toEqual('Correct!')
-  wrapper.setProps({ resultText: 'Sorry!' })
-  expect(wrapper.find('#result_text').text()).toEqual('Sorry!')
-})
-
 it('is visible or hidden based on prop value', () => {
   expect(wrapper.find('#result_card').props().className).toEqual('hidden')
   wrapper.setProps({ resultText: 'Correct!' })
   expect(wrapper.find('#result_card').props().className).toEqual('')
   wrapper.setProps({ resultText: 'Sorry!' })
   expect(wrapper.find('#result_card').props().className).toEqual('')
+})
+
+it('displays result text from prop', () => {
+  expect(wrapper.find('#result_text').text()).toEqual('')
+  wrapper.setProps({ resultText: 'Correct!' })
+  expect(wrapper.find('#result_text').text()).toEqual('Correct!')
+  wrapper.setProps({ resultText: 'Sorry!' })
+  expect(wrapper.find('#result_text').text()).toEqual('Sorry!')
 })
