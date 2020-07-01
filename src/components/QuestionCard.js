8613fe828e480470a92ca61a59d74bbf07572484
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 10:44:05 +0530

    feat: add the result card

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -3,12 +3,19 @@ import React from 'react'
 const QuestionCard = () => {
   return (
     <div id='question_card'>
-      <p id='question_text'>What was the name of the hero in the 80s animated video game 'Dragon's Lair'?</p>
+      <p id='question_text'>
+        What was the name of the hero in the 80s animated video game 'Dragon's
+        Lair'?
+      </p>
       <div id='options'>
         <button>Arthur</button>
         <button>Sir Toby Belch</button>
         <button>Guy of Gisbourne</button>
         <button>Dirk the Daring</button>
+      </div>
+      <div id='result_card'>
+        <p id='result_text'>Correct!</p>
+        <button id='next_question_button'>Next Question</button>
       </div>
     </div>
   )
