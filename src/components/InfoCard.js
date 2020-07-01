2e4a2cecb476f1a603e75ed4b25655186d2da26a
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 11:31:23 +0530

    fix: use the keyword className instead of class

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -6,12 +6,11 @@ function InfoCard () {
     <div id='info_card'>
       <h2 id='no_of_questions_answered_text'>Question 0 of 20</h2>
       <p id='question_category_text'>Entertainment: Video Games</p>
-      <div id='question_difficulty_indicator' >
-        <i class='fas fa-star highlighted' />
-        <i class='fas fa-star highlighted' />
-        <i class='fas fa-star' />
+      <div id='question_difficulty_indicator'>
+        <i className='fas fa-star highlighted' />
+        <i className='fas fa-star highlighted' />
+        <i className='fas fa-star' />
       </div>
-
     </div>
   )
 }
