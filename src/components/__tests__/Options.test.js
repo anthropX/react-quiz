9f2f44c274b8dd9b93e7745f3f6906947fc604c7
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 14:23:25 +0530

    test: ensure each option text comes from prop

--- src/components/__tests__/Options.test.js
+++ src/components/__tests__/Options.test.js
@@ -5,9 +5,32 @@ import Options from '../Options'
 let wrapper
 
 beforeEach(() => {
-  wrapper = shallow(<Options />)
+  wrapper = shallow(
+    <Options
+      options={[
+        'Arthur',
+        'Sir Toby Belch',
+        'Guy of Gisbourne',
+        'Dirk the Daring'
+      ]}
+    />
+  )
 })
 
 it('renders', () => {
   expect(wrapper.exists()).toBe(true)
+})
+
+it('displays text in each option based on prop value', () => {
+  expect(wrapper.find('#option1').text()).toEqual('Arthur')
+  expect(wrapper.find('#option2').text()).toEqual('Sir Toby Belch')
+  expect(wrapper.find('#option3').text()).toEqual('Guy of Gisbourne')
+  expect(wrapper.find('#option4').text()).toEqual('Dirk the Daring')
+  wrapper.setProps({
+    options: ['1962', '1492', '1963', '1987']
+  })
+  expect(wrapper.find('#option1').text()).toEqual('1962')
+  expect(wrapper.find('#option2').text()).toEqual('1492')
+  expect(wrapper.find('#option3').text()).toEqual('1963')
+  expect(wrapper.find('#option4').text()).toEqual('1987')
 })
