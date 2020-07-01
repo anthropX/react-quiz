fa573354475019375ba91f8676c8f36289618d0a
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 13:22:00 +0530

    test: make sure the question card has its children

--- src/components/__tests__/QuestionCard.test .js
+++ src/components/__tests__/QuestionCard.test .js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import QuestionCard from '../QuestionCard'
+import Options from '../Options'
+import ResultCard from '../ResultCard'
 
 let wrapper
 
@@ -24,11 +26,18 @@ it('renders', () => {
   expect(wrapper.exists()).toBe(true)
 })
 
+it('has the options', () => {
+  expect(wrapper.find(Options).length).toEqual(1)
+})
+
+it('has the result card', () => {
+  expect(wrapper.find(ResultCard).length).toEqual(1)
+})
+
 it('displays the question text from prop', () => {
   expect(wrapper.find('#question_text').text()).toEqual(
     "What was the name of the hero in the 80s animated video game 'Dragon's Lair'"
   )
-
   wrapper.setProps({
     questionText: 'Who is the creator of the comic series "The Walking Dead"?"'
   })
