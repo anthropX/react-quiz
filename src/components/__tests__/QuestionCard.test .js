ae41b2ba6a16079ab95b887cd7ca7ce5cea94eea
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 13:15:38 +0530

    test: ensure question text comes from props

--- src/components/__tests__/QuestionCard.test .js
+++ src/components/__tests__/QuestionCard.test .js
@@ -5,9 +5,34 @@ import QuestionCard from '../QuestionCard'
 let wrapper
 
 beforeEach(() => {
-  wrapper = shallow(<QuestionCard />)
+  wrapper = shallow(
+    <QuestionCard
+      questionText="What was the name of the hero in the 80s animated video game 'Dragon's
+  Lair'"
+      options={[
+        'Arthur',
+        'Sir Toby Belch',
+        'Guy of Gisbourne',
+        'Dirk the Daring'
+      ]}
+      resultText='Correct!'
+    />
+  )
 })
 
 it('renders', () => {
   expect(wrapper.exists()).toBe(true)
+})
+
+it('displays the question text from prop', () => {
+  expect(wrapper.find('#question_text').text()).toEqual(
+    "What was the name of the hero in the 80s animated video game 'Dragon's Lair'"
+  )
+
+  wrapper.setProps({
+    questionText: 'Who is the creator of the comic series "The Walking Dead"?"'
+  })
+  expect(wrapper.find('#question_text').text()).toEqual(
+    'Who is the creator of the comic series "The Walking Dead"?"'
+  )
 })
