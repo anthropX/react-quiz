92699f83d7f150ccd46efccb47e1cdbfba238b7f
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 22:43:38 +0530

    test: ensure max score text comes from prop

--- src/components/__tests__/ScoreCard.test.js
+++ src/components/__tests__/ScoreCard.test.js
@@ -5,9 +5,19 @@ import ScoreCard from '../ScoreCard'
 let wrapper
 
 beforeEach(() => {
-  wrapper = shallow(<ScoreCard />)
+  wrapper = shallow(<ScoreCard maxScore={100} score={0} minScore={0} />)
 })
 
 it('renders', () => {
   expect(wrapper.exists()).toBe(true)
+})
+
+it('displays the max score from props as text', () => {
+  expect(wrapper.find('#max_score_text').text()).toEqual('Max Score: 100%')
+
+  wrapper.setProps({ maxScore: 75 })
+  expect(wrapper.find('#max_score_text').text()).toEqual('Max Score: 75%')
+
+  wrapper.setProps({ maxScore: 20 })
+  expect(wrapper.find('#max_score_text').text()).toEqual('Max Score: 20%')
 })
