28162dfa6aaf5a212b598adef16bdff48393384a
Vimal Patel <94vimalpatel@gmail.com>
2020-07-01 11:39:27 +0530

    test: make sure progress bar uses prop to set size

--- src/components/__tests__/ProgressBar.test.js
+++ src/components/__tests__/ProgressBar.test.js
@@ -5,9 +5,19 @@ import ProgressBar from '../ProgressBar'
 let wrapper
 
 beforeEach(() => {
-  wrapper = shallow(<ProgressBar />)
+  wrapper = shallow(<ProgressBar progressPercentage={0} />)
 })
 
 it('renders', () => {
   expect(wrapper.exists()).toBe(true)
+})
+
+it('sizes progress fill based on prop value', () => {
+  expect(wrapper.find('#progress_bar_fill').props().style.width).toEqual('0%')
+
+  wrapper.setProps({ progressPercentage: 5 })
+  expect(wrapper.find('#progress_bar_fill').props().style.width).toEqual('5%')
+
+  wrapper.setProps({ progressPercentage: 95 })
+  expect(wrapper.find('#progress_bar_fill').props().style.width).toEqual('95%')
 })
