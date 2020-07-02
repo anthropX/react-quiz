94e249457d014b6696d765ff20dc8fc48fb15118
Vimal Patel <94vimalpatel@gmail.com>
2020-07-02 11:39:01 +0530

    test(integration): show result text on click

--- src/components/AppIntegration.test.js
+++ src/components/AppIntegration.test.js
@@ -38,3 +38,29 @@ it('resizes progress bar fill when an option is clicked', () => {
     '20%'
   )
 })
+
+it('displays "Correct!" if the option clicked is correct', () => {
+  expect(wrapper.find('#result_text').text()).toEqual('')
+
+  wrapper.find('#option4').simulate('click')
+  expect(wrapper.find('#result_text').text()).toEqual('Correct!')
+  wrapper.find('#next_question_button').simulate('click')
+
+  wrapper.find('#option4').simulate('click')
+  expect(wrapper.find('#result_text').text()).toEqual('Correct!')
+})
+
+it('displays "Sorry!" if the option clicked is incorrect', () => {
+  expect(wrapper.find('#result_text').text()).toEqual('')
+
+  wrapper.find('#option1').simulate('click')
+  expect(wrapper.find('#result_text').text()).toEqual('Sorry!')
+  wrapper.find('#next_question_button').simulate('click')
+
+  wrapper.find('#option2').simulate('click')
+  expect(wrapper.find('#result_text').text()).toEqual('Sorry!')
+  wrapper.find('#next_question_button').simulate('click')
+
+  wrapper.find('#option3').simulate('click')
+  expect(wrapper.find('#result_text').text()).toEqual('Sorry!')
+})
